1bd8f1ae836b30cc610ff47c59e12c910ed892b2
unknown <2511500082@mahasiswa.atmaluhur.ac.id>
2025-12-18 18:13:50 +0700

    menghapus 1 cid di file read.php

--- pertemuan-12/README.md
+++ pertemuan-12/README.md
@@ -9,4 +9,5 @@ Hari ini, Kamis 18 Desember 2025, saya mempelajari:
 <li>mengcopy file pertemuan 11 ke 12</li>
  <li>menambahkan tombol EDIT difile read.php</li>
  <li>memperbaiki file read.php</li>
+ <li>menghapus 1 cid di file read.php</li>
 </ol>
--- pertemuan-12/read.php
+++ pertemuan-12/read.php
@@ -16,10 +16,11 @@ $no = 1;
     <th>Pesan</th>
     <th>Created At</th>
 </tr>
-
+<?php $i = 1; ?>
 <?php while ($row = mysqli_fetch_assoc($q)) : ?>
 <tr>
-    <td><?= $no++; ?></td>
+    <td><?= $i++; ?></td>
+    <td><a href="edit.php?cid=<?= (int)$row['cid']; ?>">Edit</a></td>
     <td><?= $row['cid']; ?></td>
     <td><?= htmlspecialchars($row['cnama']); ?></td>
     <td><?= htmlspecialchars($row['cemail']); ?></td>
